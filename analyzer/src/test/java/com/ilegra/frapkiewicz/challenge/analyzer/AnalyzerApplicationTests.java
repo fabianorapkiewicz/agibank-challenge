0f01a55a2a1e721cc4fcf9dafa022b65b1dfeebb
fabianorapkiewicz <fabiano.rapkiewicz@ilegra.com>
2020-01-30 15:17:56 -0300

    Iniciando projeto

---
+++ analyzer/src/test/java/com/ilegra/frapkiewicz/challenge/analyzer/AnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilegra.frapkiewicz.challenge.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
